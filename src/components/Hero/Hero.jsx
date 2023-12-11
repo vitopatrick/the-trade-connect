364100edd69a4d052e2cf75559c20b6f536ed794
theWarlock <patrickvito2012@gmail.com>
2023-12-11 12:20:05 +0100

    added hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,25 +18,29 @@ const Hero = () => {
         }}
       >
         <div className="hero__overlay">
-          <div className="container py-5 d-flex flex-column ">
-            <div>
-              <h1 className="text-white text-center text-uppercase fs-1 fw-bold">
-                Financial Investments
-              </h1>
+          <div className="container py-5">
+            {/* header CTA */}
+            <div className="p-5">
+              <h4 className="text-white font-weight-bold fs-1 fw-bolder">
+                The Most Reliable <br /> Crypto App
+              </h4>
+              <p className="text-white">
+                Buy, store, swap and spend cryptocurrency all in one app. <br />{" "}
+                Turn Bitcoin into dollars with the BitPay crypto debit card.
+                Earn cash back automatically.
+              </p>
+              <div className="mt-4">
+                <Link to="/register" className="bg-white p-3 rounded me-4">
+                  Register today
+                </Link>
+                <a
+                  className=" bg-primary p-3 rounded text-white"
+                  href="https://play.google.com/store/apps/details?id=com.bitpay.wallet&hl=en&gl=US"
+                >
+                  Get the App
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="container">
-            <p className="text-center text-white ">
-              Bitpay Trading Signals is a group of financial and cryptocurrency
-              experts that invest in mining and cryptocurrency trading. We
-              carefully examine the volatility of bitcoin and other crypto
-              currencies, invest and make good profit from our investments.
-            </p>
-          </div>
-          <div className="text-center mx-auto">
-            <Link to="/register" className="btn btn-primary fs-5 text-sec">
-              Get Started <FaChevronCircleRight className="ms-2" />
-            </Link>
           </div>
         </div>
       </div>
