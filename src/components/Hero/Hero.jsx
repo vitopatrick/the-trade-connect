bab4febaf779332672277a825aa19adb0471268d
theWarlock <patrickvito2012@gmail.com>
2023-12-11 15:14:15 +0100

    correction

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className="hero__overlay">
           <div className="container py-5">
             {/* header CTA */}
-            <div className="p-5">
+            <div>
               <h4 className="text-white font-weight-bold fs-1 fw-bolder">
                 The Most Reliable <br /> Crypto App
               </h4>
@@ -29,12 +29,15 @@ const Hero = () => {
                 Turn Bitcoin into dollars with the BitPay crypto debit card.
                 Earn cash back automatically.
               </p>
-              <div className="mt-4">
-                <Link to="/register" className="bg-white p-3 rounded me-4">
+              <div className="mt-4 d-flex flex-column flex-sm-row">
+                <Link
+                  to="/register"
+                  className="bg-white p-3 rounded me-4 flex-fill"
+                >
                   Register today
                 </Link>
                 <a
-                  className=" bg-primary p-3 rounded text-white"
+                  className=" bg-primary p-3 rounded text-white flex-fill mt-4 mt-sm-0"
                   href="https://play.google.com/store/apps/details?id=com.bitpay.wallet&hl=en&gl=US"
                 >
                   Get the App
